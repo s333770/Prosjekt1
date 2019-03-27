5bd2f5097fcc57a842cc77927c393cf239703ea4
tm2_93@hotmail.com <Toddy2727>
2019-03-27 14:10:58 +0100

    Initial commit: Basic structure

---
+++ src/sample/Controller.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Controller {
+
+
+    public void clicked(ActionEvent actionEvent) {
+        System.out.println("Hello");
+        System.out.println("Hello");
+    }
+}
